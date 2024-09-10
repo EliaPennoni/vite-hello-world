bf2bafb6380110325c8540746d2d3aa8deaaad82
EliaPennoni <elia.pennoni1991@gmail.com>
2024-09-10 09:24:05 +0200

    esercizio termianto

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script>
+import JumbotronApp from './components/JumbotronApp.vue';
+export default {
+  setup() {
+    
+    return {
+      message:'Hello world'
+    }
+    
+  },
+  components: {
+    JumbotronApp
+  }
+}
+</script>
+
+<template>
+  <div>
+    <h1>{{ message }}</h1>
+    <jumbotronApp/>
+    
+  </div>
+  
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/JumbotronApp.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+  setup() {
+    
+    return {
+      
+    }
+    
+  }
+}
+</script>
+
+<template>
+  <div>
+    
+  </div>
+  
+</template>
+
+<style scoped>
+  div{
+    background-image: url(/vite.svg);
+    background-color:black;
+    width: 100%;
+    height: 500px;
+  }
+</style>
